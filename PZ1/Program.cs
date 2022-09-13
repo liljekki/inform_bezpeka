a22e2f4dc1fdf3565bc81f8fa65654c3392b2687
Kvartiuk <alexkvmit11@gmail.com>
2022-09-13 18:14:00 +0300

    Добавьте файлы проекта.

---
+++ PZ1/Program.cs
@@ -0,0 +1,6 @@
+﻿var rnd = new Random(90);
+for (int i = 0; i < 10; i++)
+{
+    var rnd2 = rnd.Next(0, 10);
+    Console.WriteLine(rnd2);
+}
---
+++ PZ2/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Cryptography;
+
+var rnd = new RNGCryptoServiceProvider();
+var rndNum = new byte[10];
+rnd.GetBytes(rndNum);
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(rndNum[i]);
+}
+string cryptoKey = Convert.ToBase64String(rndNum);
+Console.WriteLine(cryptoKey);
