5672d2304731bbc4007fd9612b4538461a52e2ee
liljekki <73700767+liljekki@users.noreply.github.com>
2022-10-23 16:20:47 +0300

    Add files via upload
    pz5_3

--- PZ_5/PZ_5_2/Program.cs
+++ PZ_5/PZ_5_2/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
+using System.Security.Authentication;
 using System.Security.Cryptography;
 using System.Text;
 class Program
@@ -8,17 +10,38 @@ class Program
         Console.WriteLine("Key:");
         string key = Console.ReadLine();
         Console.WriteLine("Way:");
-        int way = Convert.ToInt16(Console.ReadLine());
+        Console.WriteLine("1 - md5:\n2 - sh1:\n3 - sh256:\n4 - sh384:\n5 - sh512:\n");
+        int way = Convert.ToInt32(Console.ReadLine());
         for (int i = 0; i < 10; i++)
         {
-            HashPassword(key, 13 * 10000 + i * 50000, way);
+            HashPassword_pr(key, 13 * 10000 + i * 50000, way);
         }
     }
-    private static void HashPassword(string passwordToHash, int numberOfRounds, int way)
+    private static void HashPassword_pr(string passwordToHash, int numberOfRounds, int way)
     {
+        HashAlgorithmName method = HashAlgorithmName.MD5;
+        switch (way)
+        {
+            case 1:
+                 method = HashAlgorithmName.MD5;
+                break;
+            case 2:
+                method = HashAlgorithmName.SHA1;
+                break;
+            case 3:
+                method = HashAlgorithmName.SHA256;
+                break;
+            case 4:
+                method = HashAlgorithmName.SHA384;
+                break;
+            case 5:
+                method = HashAlgorithmName.SHA512;
+                break;
+        }
+        
         var sw = new Stopwatch();
         sw.Start();
-        var hashedPassword = PBKDF2.HashPasswordWithSalt(Encoding.UTF8.GetBytes(passwordToHash), PBKDF2.GenerateSalt(), way, numberOfRounds);
+        var hashedPassword = PBKDF2.HashPassword(Encoding.UTF8.GetBytes(passwordToHash), PBKDF2.GenerateSalt(),numberOfRounds, method);
         sw.Stop();
         Console.WriteLine();
         Console.WriteLine("Password to hash : " + passwordToHash);
@@ -26,7 +49,6 @@ class Program
         Console.WriteLine("Iterations <" + numberOfRounds + ">Elapsed Time: " + sw.ElapsedMilliseconds + "ms");
     }
 }
-
 public class PBKDF2
 {
     public static byte[] GenerateSalt()
@@ -38,61 +60,12 @@ public class PBKDF2
             return randomNumber;
         }
     }
-    private static byte[] Combine(byte[] first, byte[] second)
+    public static byte[] HashPassword(byte[] toBeHashed, byte[] salt, int numberOfRounds, HashAlgorithmName method)
     {
-        var ret = new byte[first.Length + second.Length];
-        Buffer.BlockCopy(first, 0, ret, 0, first.Length);
-        Buffer.BlockCopy(second, 0, ret, first.Length, second.Length);
-        return ret;
-    }
-    /*    public static byte[] HashPassword(byte[] toBeHashed, byte[] salt, int numberOfRounds)
-        {
-            using (var rfc2898 = new Rfc2898DeriveBytes(toBeHashed, salt, numberOfRounds))
-            {
-                return rfc2898.GetBytes(20);
-            }
-        }*/
-    public static byte[] HashPasswordWithSalt(byte[] toBeHashed, byte[] salt, int method, int numberOfRounds)
-    {
-        switch (method)
-        {
-            case 1:
-                using (var hash_way = MD5.Create())
-                {
 
-                    /*ComputeHashMd5(toBeHashed, numberOfRounds);*/
-
-                    var finnal = ComputeHashMd5(Combine(toBeHashed, salt), numberOfRounds);
-                    return finnal;
-                }
-            case 2:
-                using (var hash_way = SHA1.Create())
-                {
-                    return hash_way.ComputeHash(Combine(toBeHashed, salt));
-                }
-            case 3:
-                using (var hash_way = SHA256.Create())
-                {
-                    return hash_way.ComputeHash(Combine(toBeHashed, salt));
-                }
-            case 4:
-                using (var hash_way = SHA384.Create())
-                {
-                    return hash_way.ComputeHash(Combine(toBeHashed, salt));
-                }
-            case 5:
-                using (var hash_way = SHA512.Create())
-                {
-                    return hash_way.ComputeHash(Combine(toBeHashed, salt));
-                }
+        using (var rfc2898 = new Rfc2898DeriveBytes(toBeHashed, salt, numberOfRounds, method))
+        {
+            return rfc2898.GetBytes(20);
         }
-        return null;
-    }
-    static byte[] ComputeHashMd5(byte[] input, int rounds)
-    {
-        var md5 = MD5.Create();
-        if (rounds == 1) return md5.ComputeHash(input);
-        return md5.ComputeHash(md5.ComputeHash(input));
-
     }
 }
---
+++ PZ_5/PZ_5_3/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace hesh
+{
+    class Program
+    {
+        public static async Task Main()
+        {
+            string path = @"C:\programme\inform_bez\PZ_5\loginpasssalt.txt";
+            do
+            {
+                Console.WriteLine("Choose:");
+                Console.WriteLine("1 - register");
+                Console.WriteLine("2 - login");
+                Console.WriteLine("Write here: ");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:                        
+                        Console.WriteLine("Create login: ");
+                        string login = Console.ReadLine();
+                        Console.WriteLine("Create password: ");
+                        string password = Console.ReadLine();
+                        byte[] password_b = Encoding.Unicode.GetBytes(password);
+                        byte[] salt = Hash.GenerateSalt();
+
+                        var sha256 = Hash.HashPasswordWithSalt(password_b, salt);
+                        
+                        await File.AppendAllTextAsync(path, login + "\n");
+                        await File.AppendAllTextAsync(path, Convert.ToBase64String(sha256) + "\n");
+                        await File.AppendAllTextAsync(path, Convert.ToBase64String(salt) + "\n");
+                        break;
+                    case 2:
+                        Console.WriteLine("Login: ");
+                        string log = Console.ReadLine();
+                        Console.WriteLine("Password: ");
+                        string pass_2 = Console.ReadLine();
+                        byte[] pass_2b = Encoding.Unicode.GetBytes(pass_2);
+
+                        //var second_sha256 = ComputeHashSha256(logint_b, pass_b);
+                        //string fileText = await File.ReadAllTextAsync(path);
+                        string[] fileText = await File.ReadAllLinesAsync(path);
+
+                        int switch_in = 1;
+                        for (int i = 0; i < fileText.Length; i += 3)
+                        {
+                            byte[] salt_2 = Convert.FromBase64String(fileText[i+2]);
+                            var second_sha256 = Hash.HashPasswordWithSalt(pass_2b, salt_2);
+                            if (fileText[i + 1] == Convert.ToBase64String(second_sha256) && log == fileText[i])
+                            {
+                                switch_in++;
+                            }
+                        }
+
+                        if (switch_in > 1) Console.Write("done\n\n");
+                        else Console.WriteLine("wrong pass or login\n\n");
+
+                        break;
+                }
+            } while (true);
+        }
+            public class Hash
+        { 
+            public static byte[] GenerateSalt()
+            {
+                const int saltLength = 32;
+                using (var randomNumberGenerator =
+                new RNGCryptoServiceProvider())
+                {
+                    var randomNumber = new byte[saltLength];
+                    randomNumberGenerator.GetBytes(randomNumber);
+                    return randomNumber;
+                }
+            }
+            private static byte[] Combine(byte[] first, byte[] second)
+            {
+                var ret = new byte[first.Length + second.Length];
+                Buffer.BlockCopy(first, 0, ret, 0, first.Length);
+                Buffer.BlockCopy(second, 0, ret, first.Length,
+                second.Length);
+                return ret;
+            }
+            public static byte[] HashPasswordWithSalt(
+            byte[] toBeHashed, byte[] salt)
+            {
+                using (var sha256 = SHA256.Create())
+                {
+                    return sha256.ComputeHash(Combine(toBeHashed,
+                    salt));
+                }
+            }
+        }
+    }
+    }
