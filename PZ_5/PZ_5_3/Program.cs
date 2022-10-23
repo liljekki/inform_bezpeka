8e364d74ecf19ab689d1276f702e8722914d2833
liljekki <73700767+liljekki@users.noreply.github.com>
2022-10-23 21:55:34 +0300

    Add files via upload
    edit lab5_3

--- PZ_5/PZ_5_3/Program.cs
+++ PZ_5/PZ_5_3/Program.cs
@@ -19,16 +19,16 @@ namespace hesh
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
-                    case 1:                        
+                    case 1:
                         Console.WriteLine("Create login: ");
                         string login = Console.ReadLine();
                         Console.WriteLine("Create password: ");
                         string password = Console.ReadLine();
                         byte[] password_b = Encoding.Unicode.GetBytes(password);
-                        byte[] salt = Hash.GenerateSalt();
+                        byte[] salt = PBKDF2.GenerateSalt();
 
-                        var sha256 = Hash.HashPasswordWithSalt(password_b, salt);
-                        
+                        var sha256 = PBKDF2.HashPassword(password_b, salt, 13000);
+
                         await File.AppendAllTextAsync(path, login + "\n");
                         await File.AppendAllTextAsync(path, Convert.ToBase64String(sha256) + "\n");
                         await File.AppendAllTextAsync(path, Convert.ToBase64String(salt) + "\n");
@@ -47,8 +47,8 @@ namespace hesh
                         int switch_in = 1;
                         for (int i = 0; i < fileText.Length; i += 3)
                         {
-                            byte[] salt_2 = Convert.FromBase64String(fileText[i+2]);
-                            var second_sha256 = Hash.HashPasswordWithSalt(pass_2b, salt_2);
+                            byte[] salt_2 = Convert.FromBase64String(fileText[i + 2]);
+                            var second_sha256 = PBKDF2.HashPassword(pass_2b, salt_2, 13000);
                             if (fileText[i + 1] == Convert.ToBase64String(second_sha256) && log == fileText[i])
                             {
                                 switch_in++;
@@ -62,36 +62,24 @@ namespace hesh
                 }
             } while (true);
         }
-            public class Hash
-        { 
+        public class PBKDF2
+        {
             public static byte[] GenerateSalt()
             {
-                const int saltLength = 32;
-                using (var randomNumberGenerator =
-                new RNGCryptoServiceProvider())
+                using (var randomNumberGenerator = new RNGCryptoServiceProvider())
                 {
-                    var randomNumber = new byte[saltLength];
+                    var randomNumber = new byte[32];
                     randomNumberGenerator.GetBytes(randomNumber);
                     return randomNumber;
                 }
             }
-            private static byte[] Combine(byte[] first, byte[] second)
+            public static byte[] HashPassword(byte[] toBeHashed, byte[] salt, int numberOfRounds)
             {
-                var ret = new byte[first.Length + second.Length];
-                Buffer.BlockCopy(first, 0, ret, 0, first.Length);
-                Buffer.BlockCopy(second, 0, ret, first.Length,
-                second.Length);
-                return ret;
-            }
-            public static byte[] HashPasswordWithSalt(
-            byte[] toBeHashed, byte[] salt)
-            {
-                using (var sha256 = SHA256.Create())
+                using (var rfc2898 = new Rfc2898DeriveBytes(toBeHashed, salt, numberOfRounds))
                 {
-                    return sha256.ComputeHash(Combine(toBeHashed,
-                    salt));
+                    return rfc2898.GetBytes(20);
                 }
             }
         }
     }
-    }
+}
