08769a657d95b2d9dcf93a759726f7ee4308c18b
liljekki <73700767+liljekki@users.noreply.github.com>
2022-12-18 23:14:41 +0200

    Add files via upload
    pz_13-14

---
+++ PZ_13/PZ_13/Program.cs
@@ -0,0 +1,164 @@
+﻿using NLog;
+using System;
+using System.Data;
+using System.Data.SqlTypes;
+using System.Security;
+using System.Security.Cryptography;
+using System.Security.Principal;
+using System.Text;
+class Program
+{
+    static void Main()
+    {
+        var logger = NLog.LogManager.GetCurrentClassLogger();
+
+        logger.Trace("Register");
+        //регістр користувачів
+        string role_admin = "Admins";
+        Console.WriteLine("Registr:");
+        for (int i = 0; i < 4; i++)
+        {
+            logger.Info($"Reg user: {i}") ;
+            string[] roles = { "Users", "Junior", "Senior" };
+            if (i % 2 == 1)
+            {
+                logger.Debug($"Change first role {roles[0]} to {role_admin} ");
+                roles[0] = role_admin;   
+            };
+            var user = Protector.Register("alex" + i, "pass" + i, roles);
+            logger.Debug($"set login and pass to {"alex"+i} and {"pass"+i}");
+
+            Console.WriteLine("\n Login: " + user.Login + "\n Roles:" );
+            for (int j = 0; j < user.Roles.Length; j++)
+            {
+                Console.WriteLine("   " + user.Roles[j]);
+            }
+        }
+
+        logger.Trace("do_while loop");
+        //логін 
+        do
+        {
+            logger.Info("Enter login and pass");
+            Console.WriteLine("-----------------------------------------------------\n");
+            Console.WriteLine("Login:");
+            string login = Console.ReadLine();
+            Console.WriteLine("Password:");
+            string pass = Console.ReadLine();
+
+            
+            logger.Trace("LogIn");
+            try
+            {
+                Protector.LogIn(login, pass);
+            }
+            catch(Exception ex)
+            {
+                logger.Warn(ex, "Authentication failed");
+
+            }
+            //перевірка на роль адміна
+            logger.Info("Check pass before enter check_admin");
+            if (Protector.CheckPassword(login, pass))
+            {
+                try
+                {
+                    logger.Trace("Only for admins");
+                    Protector.OnlyForAdminsFeature();
+                }
+                catch (Exception ex)
+                {
+                    /*Console.WriteLine($"{ex.GetType()}: {ex.Message}");*/
+                    logger.Error(ex,"Admins part failed");
+                }
+            }
+            else
+            {
+                logger.Warn("wrong pass or login");
+            };
+        } while (true);
+        //NLog.LogManager.Shutdown();
+    }
+    public class Protector
+    {
+        private static Dictionary<string, User> Users = new Dictionary<string, User>();
+        public class User
+        {
+            public string Login { get; set; }
+            public string PasswordHash { get; set; }
+            public string Salt { get; set; }
+            public string[] Roles { get; set; }
+        }
+        public static User Register(string username, string password, string[] roles)
+        {
+            if (Users.ContainsKey(username))
+            {
+                Console.WriteLine("This username already registered");
+                return Users[username];
+            }
+            // генерація солі
+            var rng = RandomNumberGenerator.Create();
+            var saltBytes = new byte[16];
+            rng.GetBytes(saltBytes);
+            var saltText = Convert.ToBase64String(saltBytes);
+            // rfc
+            var hashed_pass = new Rfc2898DeriveBytes(password, saltBytes, 2000);
+            var user = new User
+            {
+                Login = username,
+                Salt = saltText,
+                PasswordHash = Convert.ToBase64String(hashed_pass.GetBytes(16)),
+                Roles = roles
+            };
+            Users.Add(user.Login, user);
+            return user;
+        }
+        public static bool CheckPassword(string username, string password)
+        {
+            //перевірка логіна у словнику
+            if (!Users.ContainsKey(username))
+            {
+                return false;
+            }
+            var user = Users[username];
+            var saltBytes = new byte[16];
+            saltBytes = Convert.FromBase64String(user.Salt);
+            var hashed_pass = new Rfc2898DeriveBytes(password, saltBytes, 2000);
+            //співставлення хешів  
+            if(Convert.ToBase64String(hashed_pass.GetBytes(16)) != user.PasswordHash)
+            {
+                return(false);
+            }
+            return (true);
+        }
+        public static void LogIn(string userName, string password)
+        {
+            // Перевірка пароля
+            if (CheckPassword(userName, password))
+            {
+                // Створюється екземпляр автентифікованого користувача
+                var identity = new GenericIdentity(userName, "OIBAuth");
+                // Виконується прив’язка до ролей, до яких належить користувач
+                var principal = new GenericPrincipal(identity, Users[userName].Roles);
+                // Створений екземпляр автентифікованого користувача з відповідними
+                // ролями присвоюється потоку, в якому виконується програма
+                System.Threading.Thread.CurrentPrincipal = principal;
+            }
+        }
+        public static void OnlyForAdminsFeature()
+        {
+            // Перевірка того, що потік програми виконується автентифікованим користувачем із певними ролями
+            if (Thread.CurrentPrincipal == null)
+            {
+                throw new SecurityException("Thread.CurrentPrincipal cannot be null.");
+            }
+            // Перевірка того, що автентифікований користувач належить до ролі "Admins"
+            if (!Thread.CurrentPrincipal.IsInRole("Admins"))
+            {
+                throw new SecurityException("User must be a member of Admins to access this feature.");
+            }
+            // У разі, якщо перевірка пройшла успішно, виконується захищена частина програми
+            Console.WriteLine("You have access to this secure feature.");
+        }
+    }
+}
