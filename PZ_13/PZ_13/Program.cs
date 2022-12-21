cbe1d7dec4088afd923108a7cc6a834eb8b77b8e
liljekki <73700767+liljekki@users.noreply.github.com>
2022-12-21 09:59:38 +0200

    Add files via upload
    pz13-14

--- PZ_13/PZ_13/Program.cs
+++ PZ_13/PZ_13/Program.cs
@@ -1,4 +1,5 @@
 ﻿using NLog;
+using NLog.Fluent;
 using System;
 using System.Data;
 using System.Data.SqlTypes;
@@ -45,39 +46,36 @@ class Program
             string login = Console.ReadLine();
             Console.WriteLine("Password:");
             string pass = Console.ReadLine();
-
             
             logger.Trace("LogIn");
             try
             {
                 Protector.LogIn(login, pass);
             }
-            catch(Exception ex)
+            catch (WrongPassword ex)
             {
-                logger.Warn(ex, "Authentication failed");
-
+                logger.Warn(ex, "Wrong pass or login");
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "Authentication failed");
             }
             //перевірка на роль адміна
             logger.Info("Check pass before enter check_admin");
-            if (Protector.CheckPassword(login, pass))
+            try
             {
-                try
-                {
-                    logger.Trace("Only for admins");
-                    Protector.OnlyForAdminsFeature();
-                }
-                catch (Exception ex)
-                {
-                    /*Console.WriteLine($"{ex.GetType()}: {ex.Message}");*/
-                    logger.Error(ex,"Admins part failed");
-                }
+                logger.Trace("Only for admins");
+                Protector.OnlyForAdminsFeature(login, pass);
             }
-            else
+            catch (WrongPassword ex)
             {
-                logger.Warn("wrong pass or login");
-            };
+                logger.Fatal(ex, "login or pass is empty");
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "Admins part failed");
+            }
         } while (true);
-        //NLog.LogManager.Shutdown();
     }
     public class Protector
     {
@@ -127,9 +125,9 @@ class Program
             //співставлення хешів  
             if(Convert.ToBase64String(hashed_pass.GetBytes(16)) != user.PasswordHash)
             {
-                return(false);
+                return (false);
             }
-            return (true);
+                return (true);
         }
         public static void LogIn(string userName, string password)
         {
@@ -144,21 +142,38 @@ class Program
                 // ролями присвоюється потоку, в якому виконується програма
                 System.Threading.Thread.CurrentPrincipal = principal;
             }
+            else
+            {
+                throw new WrongPassword("Wrong pass or login");
+            }
         }
-        public static void OnlyForAdminsFeature()
+        public static void OnlyForAdminsFeature(string login, string pass)
         {
-            // Перевірка того, що потік програми виконується автентифікованим користувачем із певними ролями
-            if (Thread.CurrentPrincipal == null)
+            if (login == "" || pass == "")
             {
-                throw new SecurityException("Thread.CurrentPrincipal cannot be null.");
+                throw new WrongPassword("login or pass - empty");
             }
-            // Перевірка того, що автентифікований користувач належить до ролі "Admins"
-            if (!Thread.CurrentPrincipal.IsInRole("Admins"))
+            if (Protector.CheckPassword(login, pass))
             {
-                throw new SecurityException("User must be a member of Admins to access this feature.");
-            }
-            // У разі, якщо перевірка пройшла успішно, виконується захищена частина програми
-            Console.WriteLine("You have access to this secure feature.");
+                // Перевірка того, що потік програми виконується автентифікованим користувачем із певними ролями
+                if (Thread.CurrentPrincipal == null)
+                {
+                    throw new SecurityException("Thread.CurrentPrincipal cannot be null.");
+                }
+                // Перевірка того, що автентифікований користувач належить до ролі "Admins"
+                if (!Thread.CurrentPrincipal.IsInRole("Admins"))
+                {
+                    throw new SecurityException("User must be a member of Admins to access this feature.");
+                }
+                // У разі, якщо перевірка пройшла успішно, виконується захищена частина програми
+                Console.WriteLine("You have access to this secure feature.");
+            } 
         }
+    }
+    public class WrongPassword : System.Exception
+    {
+        public WrongPassword() { }
+        public WrongPassword(string message) : base(message) { }
+        public WrongPassword(string message, Exception inner) : base(message, inner) { }
     }
 }
